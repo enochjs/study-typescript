a2d6cb5597d457901131df9723eff908ec425e90
enochjs <wangyongdong@haoduo.vip>
2022-07-20 23:27:05 +0800

    feat(master): more on function half

--- src/MoreOnFunctions/index.ts
+++ src/MoreOnFunctions/index.ts
@@ -163,3 +163,103 @@ function greetBad<Str extends string>(s: Str) {
 function greetGood(s: string) {
   console.log('Hello, ' + s)
 }
+
+/**
+ * Optional Parameters
+ */
+declare function f(x?: number): void
+// cut
+// All OK
+f()
+f(10)
+f(undefined)
+
+// callback
+function myForEach(arr: any[], callback: (arg: any, index?: number) => void) {
+  for (let i = 0; i < arr.length; i++) {
+    // I don't feel like providing the index today
+    callback(arr[i])
+  }
+}
+
+myForEach([1, 2, 3], (a, i) => {
+  // console.log(i.toFixed())
+})
+// When writing a function type for a callback, never write an optional parameter unless you intend to call the function without passing that argument
+
+/**
+ * Function Overloads
+ */
+//  Then, we wrote a function implementation with a compatible signature.
+// Functions have an implementation signature, but this signature can’t be called directly.
+// Even though we wrote a function with two optional parameters after the required one, it can’t be called with two parameters!
+
+function makeDate(timestamp: number): Date
+function makeDate(m: number, d: number, y: number): Date
+function makeDate(mOrTimestamp: number, d?: number, y?: number): Date {
+  if (d !== undefined && y !== undefined) {
+    return new Date(y, mOrTimestamp, d)
+  } else {
+    return new Date(mOrTimestamp)
+  }
+}
+
+const d1 = makeDate(12345678)
+const d2 = makeDate(5, 5, 5)
+// const d3 = makeDate(1, 3)
+
+// function fn1(x: string): void
+// function fn1(x: boolean): void
+// function fn1(x: string | boolean) {}
+
+function len(s: string): number
+function len(arr: any[]): number
+function len(x: any[] | string) {
+  return x.length
+}
+
+len('') // OK
+len([0]) // OK
+// len(Math.random() > 0.5 ? 'hello' : [0])
+
+/**
+ * Declaring this in a Function
+ */
+//  The JavaScript specification states that you cannot have a parameter called this,
+// and so TypeScript uses that syntax space to let you declare the type for this in the function body.
+
+interface User {
+  id: number
+  admin: boolean
+}
+
+const user = {
+  id: 123,
+  admin: false,
+  becomeAdmin: function (this: User) {
+    this.admin = true
+  },
+}
+
+user.becomeAdmin()
+const user1 = {
+  id: 123,
+  admin: false,
+}
+user.becomeAdmin.call(user1)
+
+class DB {
+  users: string[] = []
+
+  // 编译后，没有this这个参数，具体看下方调用
+  filterUsers(this: DB, name: string) {
+    this.users.filter((item) => item.includes(name))
+  }
+}
+
+const db = new DB()
+
+db.filterUsers('name')
+
+// const aa = { users: ['name', 'tag'] }
+// db.filterUsers.call(aa)
